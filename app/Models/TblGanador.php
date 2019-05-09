bf904b65b251be05c84a3f08c578c61089e68590
Christian <ChristianRogerpm@gmail.com>
2019-05-09 09:59:02 -0500

    cambios reporte ganador y sorteos

--- app/Models/TblGanador.php
+++ app/Models/TblGanador.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 
 /**
  * Class TblGanador
- * 
+ *
  * @property int $id
  * @property int $id_beneficio
  * @property int $id_cliente
@@ -25,64 +25,72 @@ use Illuminate\Http\Request;
  */
 class TblGanador extends Eloquent
 {
-	protected $table = 'tbl_ganador';
-	public $incrementing = false;
-	public $timestamps = false;
+    protected $table = 'tbl_ganador';
+    public $incrementing = false;
+    public $timestamps = false;
 
-	protected $casts = [
-		'id' => 'int',
-		'id_beneficio' => 'int',
-		'id_cliente' => 'int',
-		'id_usuario' => 'int',
-		'numeroGanador' => 'int'
-	];
+    protected $casts = [
+        'id' => 'int',
+        'id_beneficio' => 'int',
+        'id_cliente' => 'int',
+        'id_usuario' => 'int',
+        'numeroGanador' => 'int'
+    ];
 
-	protected $dates = [
-		'fecha_registro',
-		'fecha_modificacion'
-	];
+    protected $dates = [
+        'fecha_registro',
+        'fecha_modificacion'
+    ];
 
-	protected $fillable = [
-		'id_beneficio',
-		'id_cliente',
-		'id_usuario',
-		'numeroGanador',
-		'fecha_registro',
-		'fecha_modificacion'
-	];
-	public static function InsertarGanadorJson($sorteo,$numeroGanador,$cliente)
-	{
-		DB::table('tbl_ganador')->insertGetId([            
-			'id_beneficio' => $sorteo->iniciado,
-			'id_cliente' => $cliente,
-			'numeroGanador' => $numeroGanador,
-			'id_usuario' => session()->get('usuarioID'),                    
-			'fecha_registro' => date('Y-m-d'),
-		]); 
-		return true;
-	}
-	public static function ObtenerGanadorJson($beneficioId,$numeroGanador)
-	{
-		$ganador=DB::table('tbl_ganador')->where([['id_beneficio','=',$beneficioId],['numeroGanador','=',$numeroGanador]])->first();	
-        return $ganador;  
-	}
-	public static function ListarGanadorJson(Request $request)
-	{
-		if ($request->arrSorteos[0]=='0') {
-			$ganadores=DB::table('tbl_ganador')
-			->select('tbl_ganador.*','tbl_beneficios.premio','tbl_clientes.nombre','tbl_clientes.apellidoPaterno','tbl_clientes.apellidoMaterno','tbl_sorteos.nombre_sorteo')
-			->leftJoin('tbl_beneficios', 'tbl_beneficios.id', '=', 'tbl_ganador.id_beneficio')
-			->leftJoin('tbl_sorteos', 'tbl_sorteos.id', '=', 'tbl_beneficios.id_sorteo')
-			->leftJoin('tbl_clientes', 'tbl_clientes.ID', '=', 'tbl_ganador.id_cliente')
-			->get();
-		}else{
-			$ganadores=DB::table('tbl_ganador')
-			->select('tbl_ganador.*','tbl_beneficios.premio','tbl_clientes.nombre','tbl_clientes.apellidoPaterno','tbl_clientes.apellidoMaterno','tbl_sorteos.nombre_sorteo')
-			->leftJoin('tbl_beneficios', 'tbl_beneficios.id', '=', 'tbl_ganador.id_beneficio')
-			->leftJoin('tbl_sorteos', 'tbl_sorteos.id', '=', 'tbl_beneficios.id_sorteo')
-			->leftJoin('tbl_clientes', 'tbl_clientes.ID', '=', 'tbl_ganador.id_cliente')
-			->whereIn('tbl_beneficios.id_sorteo',$request->arrSorteos)->get();	
-		}				
-        return $ganadores;  
-	}	
+    protected $fillable = [
+        'id_beneficio',
+        'id_cliente',
+        'id_usuario',
+        'numeroGanador',
+        'fecha_registro',
+        'fecha_modificacion',
+        'ranking'
+    ];
+
+    public static function InsertarGanadorJson($sorteo, $numeroGanador, $cliente, $ranking)
+    {
+        DB::table('tbl_ganador')->insertGetId([
+            'id_beneficio' => $sorteo->iniciado,
+            'id_cliente' => $cliente,
+            'numeroGanador' => $numeroGanador,
+            'id_usuario' => session()->get('usuarioID'),
+            'fecha_registro' => date('Y-m-d'),
+            'ranking' => $ranking
+        ]);
+        return true;
+    }
+
+    public static function ObtenerGanadorJson($beneficioId, $numeroGanador)
+    {
+        $ganador = DB::table('tbl_ganador')->where([['id_beneficio', '=', $beneficioId], ['numeroGanador', '=', $numeroGanador]])->first();
+        return $ganador;
+    }
+
+    public static function ListarGanadorJson(Request $request)
+    {
+        if ($request->arrSorteos[0] == '0') {
+            $ganadores = DB::table('tbl_ganador')
+                ->select('tbl_ganador.*', 'tbl_beneficios.premio', 'tbl_clientes.nombre', 'tbl_clientes.apellidoPaterno', 'tbl_clientes.apellidoMaterno', 'tbl_sorteos.nombre_sorteo')
+                ->leftJoin('tbl_beneficios', 'tbl_beneficios.id', '=', 'tbl_ganador.id_beneficio')
+                ->leftJoin('tbl_sorteos', 'tbl_sorteos.id', '=', 'tbl_beneficios.id_sorteo')
+                ->leftJoin('tbl_clientes', 'tbl_clientes.ID', '=', 'tbl_ganador.id_cliente')
+                ->orderBy('tbl_ganador.id','desc')
+                ->get();
+        } else {
+            $ganadores = DB::table('tbl_ganador')
+                ->select('tbl_ganador.*', 'tbl_beneficios.premio', 'tbl_clientes.nombre', 'tbl_clientes.apellidoPaterno', 'tbl_clientes.apellidoMaterno', 'tbl_sorteos.nombre_sorteo')
+                ->leftJoin('tbl_beneficios', 'tbl_beneficios.id', '=', 'tbl_ganador.id_beneficio')
+                ->leftJoin('tbl_sorteos', 'tbl_sorteos.id', '=', 'tbl_beneficios.id_sorteo')
+                ->leftJoin('tbl_clientes', 'tbl_clientes.ID', '=', 'tbl_ganador.id_cliente')
+                ->whereIn('tbl_beneficios.id_sorteo', $request->arrSorteos)
+                ->orderBy('tbl_ganador.id','desc')
+                ->get();
+        }
+        return $ganadores;
+    }
 }
--- public/js/vistas/Reportes/ReporteGanadores.js
+++ public/js/vistas/Reportes/ReporteGanadores.js
@@ -1,38 +1,38 @@
-ArrSorteos=[];
-$(document).ready(function () {        
+ArrSorteos = [];
+$(document).ready(function () {
     llenarSorteos();
-    $('#btnGuardarSorteo').on('click', function (e) {                   
-        e.preventDefault();        
+    $('#btnGuardarSorteo').on('click', function (e) {
+        e.preventDefault();
         $('.dropdown-list1').hide('slow');
-        var arrSorteos =[]; 
-        $('.local:checkbox:checked').map(function() {
+        var arrSorteos = [];
+        $('.local:checkbox:checked').map(function () {
             arrSorteos.push(this.getAttribute("name"));
-        }).get();                        
-        if(arrSorteos.length==0){
-            toastr.error("Seleccione Local", "Mensaje del Servidor");     
+        }).get();
+        if (arrSorteos.length == 0) {
+            toastr.error("Seleccione Local", "Mensaje del Servidor");
             return false;
-        }                                                                                                                                  
+        }
         $.ajax({
             url: '/ReporteGanadoresJson',
-            data:{     
-                arrSorteos:arrSorteos,
-            }, 
+            data: {
+                arrSorteos: arrSorteos,
+            },
             type: "POST",
-            beforeSend: function () {                    
+            beforeSend: function () {
                 $.LoadingOverlay("show");
             },
-            complete: function () {                    
+            complete: function () {
                 $.LoadingOverlay("hide");
             },
-            success: function (response) {    
-                debugger                                            
+            success: function (response) {
+                debugger
                 $("#table").DataTable({
-                    dom: 'Blfrtip',  
+                    dom: 'Blfrtip',
                     buttons: [
                         {
                             extend: 'excelHtml5',
-                            title: 'Reporte Lista de Usuarios'                            
-                        },                        
+                            title: 'Reporte Lista de Usuarios'
+                        },
                         {
                             extend: 'pdfHtml5',
                             title: 'Reporte Lista de Usuarios'
@@ -50,92 +50,101 @@ $(document).ready(function () {
                     "autoWidth": false,
                     "bProcessing": true,
                     "bDeferRender": true,
-                    "lengthMenu": [ [10, 20, 50,100, -1], [10, 20, 50,100, "Todos"] ],                 
+                    "lengthMenu": [[10, 20, 50, 100, -1], [10, 20, 50, 100, "Todos"]],
                     data: response.data,
-                    columns: [                        
+                    columns: [
                         {data: "nombre_sorteo", title: "Sorteo"},
                         {data: "premio", title: "Beneficio"},
                         {data: "nombre", title: "Nombre"},
                         {data: "apellidoPaterno", title: "Apellido Paterno"},
-                        {data: "apellidoMaterno", title: "Apellido Materno"},                                              
+                        {data: "apellidoMaterno", title: "Apellido Materno"},
+                        {
+                            data: null, title: "Ranking", class: "text-center",
+                            "render":function (value) {
+                                var ranking = value.ranking == null ? '-' : value.ranking;
+                                return '<span class="badge badge-warning" style="padding-top: 7px;padding-bottom: 7px;">' + ranking + '</span>'
+                            }
+                        },
+                        {data: "fecha_registro", title: "Fecha Registro"}
                     ],
-                    "drawCallback": function (settings) {                        
+                    "drawCallback": function (settings) {
                     }
-                });    
-                $("#table_wrapper").css("overflow","scroll");                       
+                });
+                $("#table_wrapper").css("overflow", "scroll");
             },
-            error: function (jqXHR, textStatus, errorThrown) {                    
-            }                
-        });           
-    }); 
+            error: function (jqXHR, textStatus, errorThrown) {
+            }
+        });
+    });
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
-    });     
+    });
 
     //region nueva
     $('.dropdown-container1')
-	.on('click', '.dropdown-button1', function() {
-        $(this).siblings('.dropdown-list1').toggle();
-	})
-	.on('input', '.dropdown-search', function() {
-    	var target = $(this);
-        var dropdownList = target.closest('.dropdown-list1');
-    	var search = target.val().toLowerCase();
-    
-    	if (!search) {
-            dropdownList.find('li').show();
-            return false;
-        }
-    
-    	dropdownList.find('li').each(function() {
-        	var text = $(this).text().toLowerCase();
-            var match = text.indexOf(search) > -1;
-            $(this).toggle(match);
-        });
-	})
-	.on('change', '[type="checkbox"]', function() {                
-        if($(this).attr('id')=='0'){
-            $('.local:checkbox').map(function() {
-                this.checked = false;
+        .on('click', '.dropdown-button1', function () {
+            $(this).siblings('.dropdown-list1').toggle();
+        })
+        .on('input', '.dropdown-search', function () {
+            var target = $(this);
+            var dropdownList = target.closest('.dropdown-list1');
+            var search = target.val().toLowerCase();
+
+            if (!search) {
+                dropdownList.find('li').show();
+                return false;
+            }
+
+            dropdownList.find('li').each(function () {
+                var text = $(this).text().toLowerCase();
+                var match = text.indexOf(search) > -1;
+                $(this).toggle(match);
             });
-            $(this).prop('checked', true);
-        }else{
-            $('.local:checkbox').map(function() {
-                if(this.getAttribute("name")=='0'){
+        })
+        .on('change', '[type="checkbox"]', function () {
+            if ($(this).attr('id') == '0') {
+                $('.local:checkbox').map(function () {
                     this.checked = false;
-                }
-            });            
-        }  
-        var container = $(this).closest('.dropdown-container1');
-        var numChecked = container. find('[type="checkbox"]:checked').length;
-        container.find('.quantity').text(numChecked || 'Any');      
-    });              
+                });
+                $(this).prop('checked', true);
+            } else {
+                $('.local:checkbox').map(function () {
+                    if (this.getAttribute("name") == '0') {
+                        this.checked = false;
+                    }
+                });
+            }
+            var container = $(this).closest('.dropdown-container1');
+            var numChecked = container.find('[type="checkbox"]:checked').length;
+            container.find('.quantity').text(numChecked || 'Any');
+        });
 
 });
-function llenarSorteos(){                   
+
+function llenarSorteos() {
     $.ajax({
-        async:false,
+        async: false,
         type: 'post',
         url: '/SorteoListarJson',
-        data:{},            
-        success: function (response) {                                      
-            ArrSorteos=response.data;
-            response.data.unshift({ id: '0', descripcion_sorteo: 'Todos'}); 
+        data: {},
+        success: function (response) {
+            ArrSorteos = response.data;
+            response.data.unshift({id: '0', descripcion_sorteo: 'Todos'});
             var stateTemplateLocales = _.template(
                 '<li>' +
-                    '<input class="local" id="<%= id %>" name="<%= id %>" type="checkbox">' +
-                    '<label style="margin-left: 8px;" for="<%= id %>"><%= descripcion_sorteo %></label>' +
+                '<input class="local" id="<%= id %>" name="<%= id %>" type="checkbox">' +
+                '<label style="margin-left: 8px;" for="<%= id %>"><%= descripcion_sorteo %></label>' +
                 '</li>'
-            );              
-            _.each(response.data, function(s) {
+            );
+            _.each(response.data, function (s) {
                 s.descripcion_sorteo = _.startCase(s.descripcion_sorteo.toLowerCase());
                 $('#ulLocales').append(stateTemplateLocales(s));
-            }); 
-            $("#0.local").prop('checked', true);                           
+            });
+            $("#0.local").prop('checked', true);
         },
-    });      
+    });
 }
 
 
